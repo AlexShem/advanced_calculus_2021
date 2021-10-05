671b1f3ff40901e429268c220eff4d1ce0b53d3e
Aleksandr Shemendyuk <alex.shemendyuk@gmail.com>
2021-10-05 23:31:51 +0200

    Upd: logspace for x_0

--- Seminar 5 - 06.10/main_logistic.m
+++ Seminar 5 - 06.10/main_logistic.m
@@ -1,22 +1,20 @@
-c = 1/4;
-x_0 = linspace(0, 1, 20);
-T = 10;
-tspan = linspace(0, T, 100);
-
-x = zeros(length(tspan), length(x_0));
-for k = 1 : length(x_0)
-    [~, x(:, k)] = ode45(@(t, x) x - x.^2 - c, tspan, x_0(k));
-end
+c = .1;
+x_0 = logspace(-1.5, 0.8, 35);
+T = 2;
+tspan = [0, T];
 
 figure(2);
-plot(tspan, x, '-b');
+for k = 1 : length(x_0)
+    [t, x] = ode45(@(t, x) x - x.^2 - c, tspan, x_0(k));
+    plot(t, x, '-b');
+    hold on;
+end
+hold off;
 
 x_star = roots([-1 1 -c]);
-
-
 for k = 1 : length(x_star)
     if isreal(x_star(k))
         line([0, T], [x_star(k), x_star(k)], 'linestyle', '--', 'color', 'red');
     end
 end
-axis([0 T 0 1])
+axis([0 T 0 min(1, max(x_0))])
