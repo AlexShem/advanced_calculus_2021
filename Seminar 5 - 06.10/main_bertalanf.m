ec8461166291f5b3acd1ad82f74edf2a5ec50666
Aleksandr Shemendyuk <alex.shemendyuk@gmail.com>
2021-10-06 15:08:30 +0200

    Fix: multiple x_0 in ode45

--- Seminar 5 - 06.10/main_bertalanf.m
+++ Seminar 5 - 06.10/main_bertalanf.m
@@ -12,8 +12,12 @@ for k = 1 : length(V_0)
     hold on;
 end
 hold off;
+[t, V] = ode45(@(t, y) alpha*y.^(2/3) - beta*y, tspan, V_0);
+plot(t, V)
 grid on;
 xlabel('t');
 ylabel('V');
 axis([0 T 0 2.5]);
-title('$\dot{V} = \alpha V^{2/3} - \beta V$', 'Interpreter', 'latex');
+tl = title('$\dot{V} = \alpha V^{2/3} - \beta V$', 'Interpreter', 'latex');
+tl.FontSize = 14;
+subtitle(['\alpha = ' num2str(alpha), ', \beta = ', num2str(beta)]);
--- Seminar 5 - 06.10/main_logistic.m
+++ Seminar 5 - 06.10/main_logistic.m
@@ -4,12 +4,8 @@ T = 2;
 tspan = [0, T];
 
 figure(2);
-for k = 1 : length(x_0)
-    [t, x] = ode45(@(t, x) x - x.^2 - c, tspan, x_0(k));
-    plot(t, x, '-k');
-    hold on;
-end
-hold off;
+[t, x] = ode45(@(t, x) x - x.^2 - c, tspan, x_0);
+plot(t, x, '-k');
 
 x_star = roots([-1 1 -c]);
 for k = 1 : length(x_star)
@@ -17,7 +13,7 @@ for k = 1 : length(x_star)
         line([0, T], [x_star(k), x_star(k)], 'linestyle', '--', 'color', 'red');
     end
 end
-axis([0 T 0 min(1, max(x_0))])
+axis([0, T, 0, min(1, max(x_0))])
 title(['c = ' num2str(c)]);
 xlabel('t');
 ylabel('x')
