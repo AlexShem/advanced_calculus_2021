f94aac341f6268a093b016d3a84af8aef90a2c53
Aleksandr Shemendyuk <alex.shemendyuk@gmail.com>
2021-10-06 16:53:39 +0200

    Fix: prettify logistic graph

--- Seminar 5 - 06.10/main_logistic.m
+++ Seminar 5 - 06.10/main_logistic.m
@@ -1,4 +1,4 @@
-c = .1;
+c = .23;
 x_0 = logspace(-1.5, 0.8, 35);
 T = 2;
 tspan = [0, T];
@@ -10,10 +10,13 @@ plot(t, x, '-k');
 x_star = roots([-1 1 -c]);
 for k = 1 : length(x_star)
     if isreal(x_star(k))
-        line([0, T], [x_star(k), x_star(k)], 'linestyle', '--', 'color', 'red');
+        line([0, T], [x_star(k), x_star(k)], ...
+            'Linestyle', '--', 'Color', 'red', 'LineWidth', 1.6);
     end
 end
 axis([0, T, 0, min(1, max(x_0))])
-title(['c = ' num2str(c)]);
-xlabel('t');
-ylabel('x')
+tl = title('$\dot{x} = x - x^2 - c$', 'Interpreter', 'latex');
+tl.FontSize = 14;
+subtitle(['$c = $' num2str(c)], 'Interpreter', 'latex');
+xlabel('$t$', 'Interpreter', 'latex', 'FontSize', 16);
+ylabel('$x$', 'Interpreter', 'latex', 'FontSize', 16)
