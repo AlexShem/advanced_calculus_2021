c655d46e6561e39c5c6c3cebc53f43c12cc2114c
Aleksandr Shemendyuk <alex.shemendyuk@gmail.com>
2021-10-12 21:23:51 +0200

    New: Comet and Pendulum animations

--- Seminar 6 - 13.10/main_pendulum.m
+++ Seminar 6 - 13.10/main_pendulum.m
@@ -10,3 +10,22 @@ tspan = linspace(0, T, 101);
 [t, z] = ode45(@(t, z) pendulum_system(t, z, omega), tspan, z_0);
 theta = z(:, 1);
 
+%%
+figure(1);
+comet(t, theta);
+
+%%
+x_pos = sin(theta);
+y_pos = -cos(theta);
+
+figure(2);
+for k = 1 : length(t)
+    plot(L*x_pos(k), L*y_pos(k), 'ok', 'MarkerFaceColor', 'k');
+    hold on;
+    line([0 L*x_pos(k)], [0 L*y_pos(k)], 'Color', 'k');
+    hold off;
+    title(['Time = ' num2str(t(k))]);
+    axis equal;
+    axis([-L, L, -L, L]);
+    drawnow;
+end
